639d1372b135de40511cd650152f00a14da71144
Diego Cuevas <ingdcuevas@gmail.com>
2024-08-26 23:33:38 -0500

    fixing nextConfig

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,8 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname:
-          "https://s3-inventorymanagement-project.s3.us-east-2.amazonaws.com",
+        hostname: "s3-inventorymanagement-project.s3.us-east-2.amazonaws.com",
         port: "",
         pathname: "/**",
       },
